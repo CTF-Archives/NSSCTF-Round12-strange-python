2aa4326b600b3001090c0a1d629947f2ec485889
Randark_JMT <randark@foxmail.com>
2023-05-19 17:56:59 +0000

    update

--- src/preload.py
+++ src/preload.py
@@ -1,10 +1,11 @@
 import re
 import random
+from random import shuffle
 seed=114514
 random.seed(114514)
 flag="flag{test}"
 flag=re.findall(r'.{2}', flag)
-random.shuffle(flag)
+shuffle(flag)
 __flag__=flag
 del re
 del flag
